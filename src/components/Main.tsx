18d237dd7bef4effc87e5af4c2d19dfebc4cd346
Ali <s309299@studenti.polito.it>
2025-11-24 18:47:24 +0100

    updated

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -18,7 +18,7 @@ function Main() {
             <a href="https://www.linkedin.com/in/ali-arman-dai/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Ali Arman Dai</h1>
-          <p>Full Stack Engineer</p>
+          <p>Computer Engineering Student</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/armanalis" target="_blank" rel="noreferrer"><GitHubIcon/></a>
