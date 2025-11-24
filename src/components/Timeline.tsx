f690665493403bcc2610b1438fb38ceef8cf862d
Ali <s309299@studenti.polito.it>
2025-11-24 17:53:20 +0100

    images updated.

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -16,7 +16,7 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - 2026"
+            date="2022 - 2026(Expected)"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />} // You can change this icon
           >
